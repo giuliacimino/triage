091eff1e9c776f2bb58937ed5c3cc60b6aef1161
giuliacimino <giuliacimino00@gmail.com>
2023-05-26 15:33:02 +0200

    inizio progetto

---
+++ src/test/java/it/prova/triage/TriageApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.triage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
